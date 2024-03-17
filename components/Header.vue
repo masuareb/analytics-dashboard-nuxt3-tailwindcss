c8386d1480eb42a0e144b7eed522ba79f2a4cd2b
Arturo Suárez <masuareb@hotmail.com>
2024-03-16 23:25:25 -0600

    Added menu items and behavior

--- components/Header.vue
+++ components/Header.vue
@@ -24,6 +24,26 @@
           <HMenuButton class="flex h-9 w-9 shrink-0 items-center justify-center overflow-hidden rounded-full border bg-background">
             <img src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="Logged in user" class="w-full h-full">
           </HMenuButton>
+
+          <HMenuItems class="absolute right-0 z-10 mt-3 w-48 rounded-md border bg-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring">
+            <div class="border-b px-3 py-1.5 text-sm">
+              <p class="font-semibold">Hello John</p>
+              <a href="mailto:johndoe@test.com" class="leading-none text-muted-foreground">johndoe@test.com</a>
+            </div>
+
+            <div class="p-1">
+              <template v-for="(p, i) in profileMenuOptions" :key="i">
+                <HMenuItem v-if="!p.divider" v-slot="{ active }">
+                  <button 
+                    :class="[active && 'bg-muted']" 
+                    class="inline-flex w-full items-center rounded-md p-2 text-sm font-medium">
+                    {{ p.title }}
+                  </button>
+                </HMenuItem>
+                <hr v-if="p.divider" class="my-1" />
+              </template>
+            </div>
+          </HMenuItems>
         </HMenu>
       </div>
     </div>
@@ -34,5 +54,15 @@
 const mode = useColorMode();
 const toggleTheme = () => {
   mode.value = mode.value === 'light' ? 'dark' : 'light';
-}
+};
+// Items that will be displayed in menu
+const profileMenuOptions = [
+  { title: "Profile" },
+  { title: "Billing" },
+  { title: "Settings" },
+  { title: "Team members" },
+  { title: "Sales" },
+  { divider: true },
+  { title: "Logout" },
+];
 </script>
