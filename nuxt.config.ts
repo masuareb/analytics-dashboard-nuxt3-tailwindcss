36d63bacf72bcf8049e6d74933e07f62cdb213e0
Arturo Suárez <masuareb@hotmail.com>
2024-03-16 22:06:52 -0600

    Configure modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,18 @@ export default defineNuxtConfig({
     config: {},
     injectPosition: 'first',
     viewer: true,
+  },
+  headlessui: {
+    prefix: "H"
+  },
+  app: {
+    head: {
+      title: 'Analytics',
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/icon.svg" },
+        { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+        { rel: 'preconnect', href: 'https://rsms.me/' },
+      ]
+    }
   }
 })
