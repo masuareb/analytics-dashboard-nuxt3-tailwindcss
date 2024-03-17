b3270568dd666d962220186009f3f0d089f0a646
Arturo Suárez <masuareb@hotmail.com>
2024-03-16 23:07:12 -0600

    Added user profile image and menu

--- components/Header.vue
+++ components/Header.vue
@@ -9,9 +9,30 @@
         <!-- Page title -->
         <NuxtLink class="text-xl font-bold" to="/">Analytics</NuxtLink>
       </div>
+
+      <!-- Right side of the header -->
+      <div class="flex items-center gap-5">
+        <button 
+          @click="toggleTheme"
+          aria-label="Toggle theme"
+          class="flex h-9 w-9 shrink-0 items-center justify-center rounded-full border bg-background">
+          <Icon name="heroicons:sun" class="h-5 w-5"/>
+        </button>
+        
+        <!-- Profile dropdown menu -->
+        <HMenu as="div" class="relative">
+          <HMenuButton class="flex h-9 w-9 shrink-0 items-center justify-center overflow-hidden rounded-full border bg-background">
+            <img src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="Logged in user" class="w-full h-full">
+          </HMenuButton>
+        </HMenu>
+      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+const mode = useColorMode();
+const toggleTheme = () => {
+  mode.value = mode.value === 'light' ? 'dark' : 'light';
+}
 </script>
